99f3c780b43c30ba36ed09d240b93721342d7ffd
Kalo K <mega.korus@gmail.com>
2018-04-04 23:02:11 -0500

    Mi laberinto

--- Assets/Real/Movimiento.cs
+++ Assets/Real/Movimiento.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour {
 
+	public int x=0,y=0,z=0;
+
+	public Transform cam;
+	public float camX, camY, camZ;
+	float closeCamY = 0, closeCamZ = 0;
+
 	// Use this for initialization
 	void Start () {		
-		transform.position = new Vector3(1,0,0);
+		//transform.position = new Vector3(1,0,0);
+		camX = cam.transform.position.x;
+		camY = cam.transform.position.y;
+		camZ = cam.transform.position.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.anyKey) {
+			if (Input.GetKeyDown (KeyCode.W)) {
+				z++;
+				transform.position = new Vector3 (x, y, z);
+			} else if (Input.GetKeyDown (KeyCode.S)) {
+				z--;
+				transform.position = new Vector3 (x, y, z);
+			} else if (Input.GetKeyDown (KeyCode.A)) {
+				x--;
+				transform.position = new Vector3 (x, y, z);
+			} else if (Input.GetKeyDown (KeyCode.D)) {
+				x++;
+				transform.position = new Vector3 (x, y, z);
+			} else if (Input.GetAxis ("Mouse ScrollWheel") > 0f) {
+				closeCamY--;
+				closeCamZ--;
+			} else if (Input.GetAxis ("Mouse ScrollWheel") < 0f) {
+				closeCamY++;
+				closeCamZ++;
+			}
+			cam.position = new Vector3(x+camX,y+camY+closeCamY,z+camZ+closeCamZ);
+		}
+
+
+
 	}
 }
