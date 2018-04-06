70b854e4dc3e94343ebe8cf910a99eb116a29cb5
Kalo <kalo.tester@gmail.com>
2018-04-05 22:00:21 -0500

    laberinto 2

--- Assets/Real/Movimiento.cs
+++ Assets/Real/Movimiento.cs
@@ -4,46 +4,102 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour {
 
-	public int x=0,y=0,z=0;
+	public float speed;
+	public Vector3 direction;
+
+	public Vector3 limit;
 
 	public Transform cam;
-	public float camX, camY, camZ;
-	float closeCamY = 0, closeCamZ = 0;
+	float camX, camY, camZ;
+
+	float timer = 1;
 
 	// Use this for initialization
 	void Start () {		
 		//transform.position = new Vector3(1,0,0);
-		camX = cam.transform.position.x;
-		camY = cam.transform.position.y;
-		camZ = cam.transform.position.z;
+		if(cam!=null){
+			camX = cam.transform.position.x;
+			camY = cam.transform.position.y;
+			camZ = cam.transform.position.z;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.anyKey) {
-			if (Input.GetKeyDown (KeyCode.W)) {
-				z++;
-				transform.position = new Vector3 (x, y, z);
-			} else if (Input.GetKeyDown (KeyCode.S)) {
-				z--;
-				transform.position = new Vector3 (x, y, z);
-			} else if (Input.GetKeyDown (KeyCode.A)) {
-				x--;
-				transform.position = new Vector3 (x, y, z);
-			} else if (Input.GetKeyDown (KeyCode.D)) {
-				x++;
-				transform.position = new Vector3 (x, y, z);
-			} else if (Input.GetAxis ("Mouse ScrollWheel") > 0f) {
-				closeCamY--;
-				closeCamZ--;
-			} else if (Input.GetAxis ("Mouse ScrollWheel") < 0f) {
-				closeCamY++;
-				closeCamZ++;
-			}
-			cam.position = new Vector3(x+camX,y+camY+closeCamY,z+camZ+closeCamZ);
+
+		/*if (transform.position.x >= limit) {
+			speed = 0;
+			transform.position = new Vector3 (limit, transform.position.y, transform.position.z);
 		}
 
+		transform.Translate (direction * speed * Time.deltaTime);
+		cam.position = new Vector3(transform.position.x+camX,transform.position.y+camY+closeCamY,transform.position.z+camZ+closeCamZ);*/
 
+		/*Vector3 currentMovement = Vector3.zero;
+
+		if (transform.position.x < limit.x) {
+			currentMovement.x = direction.x * speed; 
+		} 
+
+		if (transform.position.y < limit.y) {
+			currentMovement.y = direction.y * speed;
+		}
+
+		if (transform.position.z < limit.z) {
+			currentMovement.z = direction.z * speed;
+		}
+
+		transform.Translate (currentMovement * Time.deltaTime);
+		cam.position = new Vector3(transform.position.x+camX,transform.position.y+camY+closeCamY,transform.position.z+camZ+closeCamZ);
+		*/
+
+
+
+		if (Input.anyKey) {
+			if (Input.GetKey (KeyCode.W) && transform.position.z < limit.x) {
+				transform.position += new Vector3 (0, 0, speed * Time.deltaTime);
+
+				//transform.Rotate (new Vector3 (speed*15, 0, 0) * Time.deltaTime);
+			} else if (Input.GetKey (KeyCode.S) && transform.position.z > -limit.z) {
+				transform.position += new Vector3 (0, 0, -speed * Time.deltaTime);
+
+				//transform.Rotate (new Vector3 (-speed*15, 0, 0) * Time.deltaTime);
+			} 
+			if (Input.GetKey (KeyCode.A) && transform.position.x > -limit.x) {
+				transform.position += new Vector3 (-speed * Time.deltaTime, 0, 0);
+
+				//transform.Rotate (new Vector3 (0, 0, speed*15) * Time.deltaTime);
+
+			} 
+			if (Input.GetKey (KeyCode.D) && transform.position.x < limit.x) {
+				transform.position += new Vector3 (speed * Time.deltaTime,0 ,0);
+
+				//transform.Rotate (new Vector3 (0, 0, -speed*15) * Time.deltaTime);
+
+			} 
+			//cam.position = new Vector3(x+camX,y+camY+closeCamY,z+camZ+closeCamZ);
+		}
+
+		/*Timer delta*/
+		/*timer -= Time.deltaTime;
+		if (timer <= 0) {
+			//action
+		}*/
+
+		//transform.position = Vector3.MoveTowards (transform.position, limit, speed * Time.deltaTime);
+		if (transform.position.x == limit.x) {
+			limit.x *= -1;
+		}
+
+		if (transform.position.y == limit.y) {
+			limit.y *= -1;
+		}
+
+		if (transform.position.z == limit.z) {
+			limit.z *= -1;
+		}
+
+		if(cam!=null) cam.position = new Vector3(transform.position.x+camX,transform.position.y+camY,transform.position.z+camZ);
 
 	}
 }
